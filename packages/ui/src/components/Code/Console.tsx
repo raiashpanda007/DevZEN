8da4504821fdf6caa887f12d98f8e298a31948a8
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-04-02 16:41:54 +0530

    Feat: Add Console and Run components; integrate them into MonacoEditor layout

---
+++ packages/ui/src/components/Code/Console.tsx
@@ -0,0 +1,12 @@
+import {Button} from "@workspace/ui/components/button";
+import { Terminal } from "lucide-react";
+
+function Console() {
+  return (
+    <Button>
+        Console <Terminal/>
+    </Button>
+  )
+}
+
+export default Console
--- packages/ui/src/components/Code/MonacoEditor.tsx
+++ packages/ui/src/components/Code/MonacoEditor.tsx
@@ -1,39 +1,51 @@
-"use client"
+"use client";
 import { useRef, useEffect, useState } from "react";
 import { Editor, OnMount } from "@monaco-editor/react";
 import { useTheme } from "next-themes";
 import * as monaco from "monaco-editor";
-
+import Run from "@workspace/ui/components/Code/Run";
+import Console from "@workspace/ui/components/Code/Console";
 const MonacoEditor = () => {
   const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
   const [theme, setTheme] = useState("vs-dark");
   const currTheme = useTheme().theme;
-  useEffect(() =>{
-   
+  useEffect(() => {
     if (currTheme === "dark") {
       setTheme("vs-dark");
     } else {
       setTheme("vs-light");
     }
-  })
+  });
 
   const handleEditorMount: OnMount = (editor, monaco) => {
     editorRef.current = editor;
   };
 
   return (
-    <Editor
-      height="790px"
-      defaultLanguage="javascript"
-      defaultValue="// Write your code here... "
-      theme={theme}
-      options={{
-        fontSize: 14,
-        minimap: { enabled: true },
-        automaticLayout: true,
-      }}
-      onMount={handleEditorMount}
-    />
+    <div className="relative ">
+    
+      <div className="w-full">
+        <Editor
+          height="750px"
+          defaultLanguage="javascript"
+          defaultValue="// Write your code here... "
+          theme={theme}
+          options={{
+            fontSize: 14,
+            minimap: { enabled: true },
+            automaticLayout: true,
+          }}
+          onMount={handleEditorMount}
+        />
+      </div>
+      
+        <div className="flex w-full">
+        <Run />
+        <Console/>
+
+        </div>
+      
+    </div>
   );
 };
 
