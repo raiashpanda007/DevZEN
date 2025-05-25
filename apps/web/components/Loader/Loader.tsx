147949b7d067f71209f72cdea469bc7e69775eeb
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-05-25 23:18:45 +0530

    Just update the look of  loader

--- apps/web/components/Loader/Loader.tsx
+++ apps/web/components/Loader/Loader.tsx
@@ -3,7 +3,7 @@
 function Loader() {
   return (
     <div
-  className="w-12 h-12 border-8 border-t-transparent dark:border-t-transparent border-black dark:border-white rounded-full animate-spin "
+  className="w-12 h-12 border-4 border-t-transparent dark:border-t-transparent border-black dark:border-white rounded-full animate-spin "
   
 ></div>
 
