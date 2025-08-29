bf86dafdc56bb51f5af6eb40b2ec0733551c30d7
Ubuntu <ubuntu@ip-172-31-43-16.ap-south-1.compute.internal>
2025-08-29 17:57:28 +0000

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      name: "devzen-web",
+      cwd: "./apps/web",
+      script: "npm",
+      args: "start",
+      interpreter: "/home/ubuntu/.nvm/versions/node/v22.12.0/bin/node",
+    },
+    {
+      name: "devzen-server",
+      cwd: "./apps/Server",
+      script: "npm",
+      args: "start",
+      interpreter: "/home/ubuntu/.nvm/versions/node/v22.12.0/bin/node",
+    },
+    {
+      name: "deevzen-cleaner",
+      cwd: "./apps/Jobs",
+      script: "npm",
+      args: "start",
+      interpreter: "/home/ubuntu/.nvm/versions/node/v22.12.0/bin/node",
+    },
+
+  ],
+};
