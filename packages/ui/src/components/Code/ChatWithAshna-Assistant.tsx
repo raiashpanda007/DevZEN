c5c8c405dd76f3a8b5987558e95c7f7f89e96ffc
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-09-08 14:15:30 +0530

    feat: add ChatWithAshna-Assistant component and update MonacoEditor import

--- packages/ui/src/components/Code/ChatWithAshna-Assistant.tsx
+++ packages/ui/src/components/Code/ChatWithAshna-Assistant.tsx
@@ -1,11 +1,11 @@
 "use client"
 
 import { Button } from "@workspace/ui/components/button";
-import { Play } from "lucide-react";
+
 const Run = () => {
   return (
     <Button className="bg-green-600 flex">
-      Run <Play />
+      Chat with Ashna 
     </Button>
   );
 };
